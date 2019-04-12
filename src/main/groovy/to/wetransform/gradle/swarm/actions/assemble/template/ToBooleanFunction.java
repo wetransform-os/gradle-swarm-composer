a798f74455aba29237e2acb950fc03bbdeed3535
Simon Templer <simon@wetransform.to>
2019-04-12 13:46:42 +0200

    feat: add functions for conversion to integer, double and boolean

---
+++ src/main/groovy/to/wetransform/gradle/swarm/actions/assemble/template/ToBooleanFunction.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 wetransform GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package to.wetransform.gradle.swarm.actions.assemble.template;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.mitchellbosecke.pebble.extension.Function;
+
+/**
+ * Simple function that tries to convert a value to a boolean.
+ *
+ * @author Simon Templer
+ */
+public class ToBooleanFunction implements Function {
+
+  @Override
+  public List<String> getArgumentNames() {
+    return Collections.singletonList("value");
+  }
+
+  @Override
+  public Boolean execute(Map<String, Object> args) {
+    Object value = args.get("value");
+    if (value instanceof Boolean) {
+      return (Boolean) value;
+    }
+    else if (value instanceof String) {
+      String lower = ((String) value).toLowerCase();
+      if (lower.equals("true")) {
+        return true;
+      }
+      if (lower.equals("false")) {
+        return false;
+      }
+    }
+
+    throw new IllegalArgumentException("Cannot convert value to boolean: " + value);
+  }
+
+}
